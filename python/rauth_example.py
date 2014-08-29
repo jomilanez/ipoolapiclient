ae759b42d5b507c1c1c6b3e0063536726f372e68
Jan Nabbefeld <jan.nabbefeld@kreuzwerker.de>
2014-08-29 11:33:08 +0200

    MusicHackDay credentials added.

--- python/rauth_example.py
+++ python/rauth_example.py
@@ -2,8 +2,10 @@
 from rauth import OAuth1Session
 
 # only consumer_key and consumer_secret needs to be defined 
-session = OAuth1Session( consumer_key='oauth-key', consumer_secret='oauth-secret')
+session = OAuth1Session(consumer_key='musichackday2014', consumer_secret='276ea410-29fb-11e4-8c21-0800200c9a66')
 # it's important to enable header based authentification, URL parameter signature will not work
-r = session.get('https://ipool.s.asideas.de/api/v3/search', header_auth=True)
+# to ignore SSL certificate verify failed error by setting verify parameter to False
+r = session.get('https://ipool.s.asideas.de/api/v3/search', header_auth=True, verify=False)
 # r is an Response object; if you get <Response [200]> you're good
+# have a look at https://github.com/kennethreitz/requests
 print r
