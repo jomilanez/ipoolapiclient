73f7bed3b0227204801e84fab14afb60c73a8f9d
slehrack <sebastian.lehrack@asideas.de>
2014-10-10 11:56:56 +0200

    API key for FinHackathonAPI key for FinHackathonAPI key for FinHackathonAPI key for FinHackathon

--- ruby/oauth_example.rb
+++ ruby/oauth_example.rb
@@ -3,8 +3,8 @@
 require 'rubygems'
 require 'oauth'
 
-consumer_key='musichackday2014'
-consumer_secret='276ea410-29fb-11e4-8c21-0800200c9a66'
+consumer_key='hackathon'
+consumer_secret='33f3fb11-59b4-4a38-a549-27bb4628d1af'
 site = 'https://ipool.s.asideas.de'
 
 @consumer=OAuth::Consumer.new consumer_key,
