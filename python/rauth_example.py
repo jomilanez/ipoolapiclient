07534f2a432cd80c4104d063090dc46570b06b9e
slehrack <sebastian.lehrack@asideas.de>
2014-10-10 11:59:36 +0200

    API key for FinHackathon

--- python/rauth_example.py
+++ python/rauth_example.py
@@ -2,7 +2,7 @@
 from rauth import OAuth1Session
 
 # only consumer_key and consumer_secret needs to be defined 
-session = OAuth1Session(consumer_key='musichackday2014', consumer_secret='276ea410-29fb-11e4-8c21-0800200c9a66')
+session = OAuth1Session(consumer_key='hackathon', consumer_secret='33f3fb11-59b4-4a38-a549-27bb4628d1af')
 # it's important to enable header based authentification, URL parameter signature will not work
 # to ignore SSL certificate verify failed error by setting verify parameter to False
 r = session.get('https://ipool.s.asideas.de/api/v3/search', header_auth=True, verify=False)
